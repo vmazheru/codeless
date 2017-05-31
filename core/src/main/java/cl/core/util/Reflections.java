8c7390e44a09b8f197f34f1a2e09d5a394fd35c5
Vladimir Mazheru <v_mazheru@yahoo.com>
2017-05-30 20:43:50 -0400

    Implemented some new reflection methods

--- core/src/main/java/cl/core/util/Reflections.java
+++ core/src/main/java/cl/core/util/Reflections.java
@@ -1,8 +1,12 @@
 package cl.core.util;
 
-import static cl.core.decorator.exception.ExceptionDecorators.*;
+import static cl.core.decorator.exception.ExceptionDecorators.uncheck;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import cl.core.function.FunctionWithException;
+import cl.core.function.SupplierWithException;
 
 /**
  * Reflection utilities.
@@ -10,6 +14,7 @@ import java.lang.reflect.Method;
  * <p>Any checked exceptions thrown by reflection API will be
  * wrapped in run-time exceptions in all methods of this class.
  */
+@SuppressWarnings("unchecked")
 public final class Reflections {
     
     private Reflections(){}
@@ -17,16 +22,133 @@ public final class Reflections {
     /**
      * Execute a setter.
      * 
-     * @param propertyName   A property name to set. This is not a method name, but lower-cased property name.
+     * @param propertyName   A property name to set. This is not a method name, but a lower-cased property name.
      *                       (For example, it's not "setFirstName" but "firstName") 
      * @param propertyValue  A property value
      * @param object         An object, whose property we're setting.
      */
     public static void set(String propertyName, Object propertyValue, Object object) {
-        uncheck(() -> {
-            Method setter = object.getClass().getMethod(
-                    "set" + Strings.capitalize(propertyName), propertyValue.getClass());
-            setter.invoke(object, propertyValue);
-        });
+        call("set" + Strings.capitalize(propertyName), object, propertyValue);
     }
+    
+    /**
+     * Execute a getter.
+     * 
+     * @param propertyName A property name. This is not a method name, but a lower-cased property name.
+     * @param object       An object on which the method is being called.
+     * @return             A property value
+     */
+    public static <T> T get(String propertyName, Object object) {
+        return call("get" + Strings.capitalize(propertyName), object);
+    }
+    
+    /**
+     * Call a method which takes no arguments on a given object.
+     * 
+     * @param method A method name
+     * @param object An object on which the method is being called
+     * @return       The result of the method call
+     */
+    public static <T> T call(String method, Object object) {
+        return (T)call(
+                () -> object.getClass().getDeclaredMethod(method),
+                m -> m.invoke(object));
+    }
+    
+    /**
+     * Call a method which takes one argument on a given object.
+     * 
+     * @param method A method name
+     * @param object An object on which the method is being called
+     * @param arg    A method argument
+     * @return       The result of the method call
+     */
+    public static <T> T call(String method, Object object, Object arg) {
+        return (T)call(
+                () -> object.getClass().getDeclaredMethod(method, arg.getClass()),
+                m -> m.invoke(object, arg));
+    }
+    
+    /**
+     * Call a method which takes two arguments on a given object.
+     * 
+     * @param method A method name
+     * @param object An object on which the method is being called
+     * @param arg1   The first method argument
+     * @param arg2   The second method argument
+     * @return       The result of the method call
+     */
+    public static <T> T call(String method, Object object, Object arg1, Object arg2) {
+        return (T)call(
+                () -> object.getClass().getDeclaredMethod(method, 
+                            arg1.getClass(), arg2.getClass()),
+                m -> m.invoke(object, arg1, arg2));
+    }
+    
+    /**
+     * Call a method which takes three arguments on a given object.
+     * 
+     * @param method A method name
+     * @param object An object on which the method is being called
+     * @param arg1   The first method argument
+     * @param arg2   The second method argument
+     * @param arg3   The third method argument
+     * @return       The result of the method call
+     */
+    public static <T> T call(String method, Object object, 
+            Object arg1, Object arg2, Object arg3) {
+        return (T)call(
+                () -> object.getClass().getDeclaredMethod(method, 
+                            arg1.getClass(), arg2.getClass(), arg3.getClass()),
+                m -> m.invoke(object, arg1, arg2, arg3));
+    }
+    
+    /**
+     * Call a method which take variable number of arguments on a given object.
+     * 
+     * @param method A method name    
+     * @param object An object on which the method is being called
+     * @param args   Method arguments. Note that, the arguments are of the same type
+     * @return       The result of the method call
+     */
+    @SafeVarargs
+    public static <T,P> T call(String method, Object object, P ... args) {
+        return (T)call(() -> object.getClass().getDeclaredMethod(method, args.getClass()),
+                m -> m.invoke(object, new Object[] {args}));
+    }
+
+    /**
+     * Get a value directly from an object field.
+     * 
+     * @param fieldName Field name
+     * @param object    An object whose value we're getting
+     * @return          The field value
+     */
+    public static <T> T getField(String fieldName, Object object) {
+        return (T)uncheck(() -> getFld(fieldName, object).get(object));
+    }
+    
+    /**
+     * Set a field value directly.
+     * 
+     * @param fieldName  Field name 
+     * @param fieldValue Filed value
+     * @param object     An object whose value we're setting
+     */
+    public static void setField(String fieldName, Object fieldValue, Object object) {
+        uncheck(() -> getFld(fieldName, object).set(object, fieldValue));
+    }
+    
+    private static Field getFld(String fieldName, Object object) throws NoSuchFieldException {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field;
+    }
+    
+    private static <T> T call(
+            SupplierWithException<Method> methodSupplier, 
+            FunctionWithException<Method, T> methodToResultFunction) {
+        return uncheck(() -> methodToResultFunction.apply(methodSupplier.get()));
+    }
+    
 }
--- core/src/test/java/cl/core/util/ReflectionsTest.java
+++ core/src/test/java/cl/core/util/ReflectionsTest.java
@@ -12,22 +12,15 @@ public class ReflectionsTest {
 
     @Test
     public void testSet() {
-        
-        class ObjectWithSetter {
-            private String value;
-            public String getValue() { return value; }
-            @SuppressWarnings("unused")
-            public void setValue(String value) { this.value = value; }
-        }
-        
         // test normally
-        ObjectWithSetter obj = new ObjectWithSetter();
+        SampleObject obj = new SampleObject();
         set("value", "some value", obj);
         assertEquals("some value", obj.getValue());
         
         // call the setter for non existing property
         try {
             set("wrongProperty", "some value", obj);
+            fail("Setting wrong property must result in NoSuchMethodException");
         } catch (RuntimeException e) {
             assertTrue(e.getCause() instanceof NoSuchMethodException);
         }
@@ -35,10 +28,71 @@ public class ReflectionsTest {
         // call the setter with wrong data type
         try {
             set("value", 42, obj);
+            fail("Setting property to the value of wrong data type must result in NoSuchMethodException");
         } catch (RuntimeException e) {
             assertTrue(e.getCause() instanceof NoSuchMethodException);
         }
+    }
+    
+    @Test
+    public void testGet() {
+        SampleObject obj = new SampleObject();
+        obj.setValue("my value");
+        assertEquals(obj.getValue(), get("value", obj));
+    }
+    
+    @Test
+    public void testSetField() {
+        final String value = "my value"; 
+        SampleObject obj = new SampleObject();
+        setField("value", value, obj);
+        assertEquals(value, obj.getValue() );
+    }
+    
+    @Test
+    public void testGetField() {
+        SampleObject obj = new SampleObject();
+        obj.setValue("my value");
+        assertEquals(obj.getValue(), getField("value", obj));
+    }
+    
+    @Test
+    public void testCall() {
+        final String value = "abcdef";
+        SampleObject obj = new SampleObject();
+        obj.setValue(value);
+        String result = call("append", obj);
+        assertEquals(value + value, result);
         
+        obj.setValue(value);
+        result = call("append", obj, "123");
+        assertEquals(value + "123", result);
+        
+        obj.setValue(value);
+        result = call("append", obj, "123", "456");
+        assertEquals(value + "123456", result);
+        
+        obj.setValue(value);
+        result = call("append", obj, "123", "456", "789");
+        assertEquals(value + "123456789", result);
+        
+        obj.setValue(value);
+        assertEquals(value + "123ABC_.txt", call("append", obj, "123", "ABC", "_", ".txt"));
+    }
+    
+    static class SampleObject {
+        private String value;
+        public String getValue() { return value; }
+        public void setValue(String value) { this.value = value; }
+        
+        public String append() { return append(value); }
+        public String append(String other) { value += other; return value; }
+        public String append(String s1, String s2) { value += s1; value += s2; return value; }
+        public String append(String s1, String s2, String s3) { value += s1; value += s2; value += s3; return value; }
+        public String append(String ... others) {
+            for (String s : others) append(s) ; 
+            return value;
+        }
     }
     
 }
