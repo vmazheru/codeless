aa8bed3370cd29d1c5cd72573547dd19a8a7af94
Vladimir Mazheru <v_mazheru@yahoo.com>
2018-01-19 17:57:04 -0500

    Added java doc

--- core/src/main/java/cl/core/types/Transient.java
+++ core/src/main/java/cl/core/types/Transient.java
@@ -7,6 +7,10 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Fields and methods (setters and getters) marked this annotation
+ * will not be processed by reflection utilities from {@link cl.core.util.Reflections} class.
+ */
 @Target({FIELD, METHOD})
 @Retention(RUNTIME)
 public @interface Transient {}
